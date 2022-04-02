25714d92c6687852f1b739dce81294bae785a9e2
김성현 <61579430+kim70273@users.noreply.github.com>
2022-04-02 16:49:46 +0900

    as를 통해서 스타일은 유지한채 태그 바꾸기, attrs를 통해서 모든 컴포넌트에 공통되는 속성 부여하기.

--- src/App.js
+++ src/App.js
@@ -19,11 +19,33 @@ const Text = styled.span`
   color: white;
 `;
 
+// props에 as를 넘겨주면 스타일은 유지하면서 html 태그만을 바꿀 수도 있다.
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+// attrs를 통해서 Input으로 전달될 속성 오브젝트를 담아 줄 수 있다
+//  ex. 이렇게 하면 컴포넌트 하나하나에 required를 적어 주지 않아도 됨.
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
+`;
+
 function App() {
   return (
     <Father>
       <Box bgColor="teal" />
       <Circle bgColor="tomato" />
+      <Btn>Login</Btn>
+      <Btn as="a" href="/">
+        Login
+      </Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
