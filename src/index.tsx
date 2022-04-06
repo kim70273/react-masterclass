92160e0b5740cc2edf63de5bb3fdc114dac733ac
김성현 <61579430+kim70273@users.noreply.github.com>
2022-04-06 18:55:10 +0900

    기존 프로젝트에 타입스크립트 설정.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
+// import한 자바스크립트 라이브러리가 무엇인지 타립스크립트에게 설명해주기 위해
+// npm i --save-dev @types/styled-components 를 설치 하였다.
 import App from "./App";
 
 // darkTheme과 lightTheme은 프로퍼티 이름들이 같아야 한다.
