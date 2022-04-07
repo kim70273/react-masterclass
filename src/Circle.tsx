08e565d9f7a2b5b2c859540b506576645917d35c
김성현 <61579430+kim70273@users.noreply.github.com>
2022-04-08 01:52:44 +0900

    state의 타입 지정해주기.

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 // 타입스크립트가 봤을때 Container은 div일 뿐이므로
@@ -30,6 +30,11 @@ const Circle = ({
   borderColor,
   text = "default text",
 }: CircleProps) => {
+  //<..>안에 상태가 가질수 있는 값이 어떤 타입인지 작성해준다.
+  const [value, setValue] = useState<number | string>(0);
+  setValue(2);
+  setValue("hello");
+
   // ??를 톨해서 만약 borderColor가 undefined 상태라면 bgColor 값이 들어가게된다. (디폴트값을 주는 또 하나의 방법)
   // 이렇게 사용할 수 있는 이유는 bgColor는 required로 항상 string이기 때문이다.
   return (
