52c761a960bb9829b202fd1a97011532cf12c61a
김성현 <61579430+kim70273@users.noreply.github.com>
2022-04-28 01:58:01 +0900

    Chart 컴포넌트를 위한 fetcher함수 만들고, useQuery로 이용.

--- src/api.ts
+++ src/api.ts
@@ -17,3 +17,11 @@ export function fetchCoinTickers(coinId: string | undefined) {
     response.json()
   );
 }
+
+export function fetchCoinHistory(coinId: string | undefined) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2; // 14일 전이 시작 날.
+  return fetch(
+    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+  ).then((response) => response.json());
+}
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
 
-const Chart = () => {
+interface ChartProps {
+  coinId: string | undefined;
+}
+
+const Chart = ({ coinId }: ChartProps) => {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+
+  console.log(data);
   return <h1>Chart</h1>;
 };
 
