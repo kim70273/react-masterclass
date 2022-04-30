cd9d8077a9d057778e0d7a7727ceaf4fc79380aa
김성현 <61579430+kim70273@users.noreply.github.com>
2022-04-30 23:00:11 +0900

    ApexChart 그래프 좀 더 예쁘게 수정1

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,18 +1,91 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string | undefined;
 }
 
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 const Chart = ({ coinId }: ChartProps) => {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
 
-  console.log(data);
-  return <h1>Chart</h1>;
+  // 차트에 왼쪽에 숫자를 없애고, 밑에숫자는 날짜가 나오도록 수정하기
+  return (
+    <div>
+      {isLoading ? (
+        "Loading Chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data?.map((price) => price.close) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              width: 500,
+              height: 300,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              labels: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Chart;
